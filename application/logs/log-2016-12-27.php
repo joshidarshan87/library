3847401e9229ab4a78258f2f7af8b86a9f0d0315
Kishan <Kishan@Kishan-PC>
2016-12-27 10:43:45 +0530

    search functionality done

---
+++ application/logs/log-2016-12-27.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-12-27 10:36:20 --> 404 Page Not Found: Assets/font_awesome
+ERROR - 2016-12-27 10:36:20 --> 404 Page Not Found: Assets/dist
+ERROR - 2016-12-27 10:36:20 --> 404 Page Not Found: Assets/metisMenu
+ERROR - 2016-12-27 10:36:20 --> 404 Page Not Found: Assets/metisMenu
+ERROR - 2016-12-27 10:36:20 --> 404 Page Not Found: Assets/dist
+ERROR - 2016-12-27 10:36:20 --> 404 Page Not Found: Faviconico/index
